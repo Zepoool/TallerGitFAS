2a21a955fecf7ee53783563e77c12bb062f84e14
Zepoool <fablopezsierra@gamil.com>
2025-09-24 11:30:39 -0500

    Primer commit con main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Proyecto: Taller Git y GitHub
+Equipo: Equipo FAS
+Descripción: Programa de operaciones matemáticas simples en Python.
+"""
+
+# Funciones básicas
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+# Bloque principal
+if __name__ == "__main__":
+    # Variables con valores asignados
+    a = 12
+    b = 4
+
+    print("=== Proyecto Taller Git & GitHub ===")
+    print(f"Suma {a} + {b} = {suma(a, b)}")
+    print(f"Resta {a} - {b} = {resta(a, b)}")
